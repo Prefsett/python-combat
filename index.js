192abb8f2caba4b384ddec712744c7400068409f
Prefsett <oleg2008@gmail.com>
2024-06-15 21:25:09 +0300

    feat

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { TelegramBot } = require("./bot");
+const { TelegramBot } = require("./bot/bot.js");
 
 const bot = new TelegramBot(process.env.TOKEN);
 bot.start();
---
+++ src/scripts/main.js
@@ -0,0 +1,18 @@
+import { Counter } from "./localstorage/counter.js";
+
+export class App {
+  static start() {
+    const clickedElement = document.getElementById("clicked");
+    const counterElement = document.getElementById("result");
+
+    let counter = Counter.get();
+
+    const render = () => (counterElement.innerHTML = `Coins ${counter}`);
+    render();
+
+    clickedElement.addEventListener("click", (evt) => {
+      Counter.set(++counter);
+      render();
+    });
+  }
+}
