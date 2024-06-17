7cbe1b73cd8c2fb15e2484227bba7c1e28cdb493
Prefsett <oleg2008@gmail.com>
2024-06-17 21:54:23 +0300

    feat

--- bot/bot.js
+++ bot/bot.js
@@ -6,21 +6,23 @@ class TelegramBot {
     if (TelegramBot.instance) return TelegramBot.instance;
     TelegramBot.instance = this;
 
-    this.data = new Data("user.json");
-    this.data.getData();
-
     this.bot = new Bot(TOKEN);
     this.urlApp = "t.me/pityhon_combat_bot/webapp";
 
+    this.data = new Data("user.json");
+
     this.bot.command("start", async (ctx) => {
       console.log(ctx.from);
-      await this.data.setData(ctx.from);
 
-      await ctx.reply("Привет", {
+      const massage = `Hello, @${ctx.from.username}, u can start play Python-Combat now. Start playing...`;
+      const replyObj = {
         reply_markup: {
-          inline_keyboard: [[{ text: "Начать играть", url: this.urlApp }]],
+          inline_keyboard: [[{ text: "Start play", url: this.urlApp }]],
         },
-      });
+      };
+
+      await this.data.setData(ctx.from);
+      await ctx.reply(massage, replyObj);
     });
   }
 
--- bot/data.js
+++ bot/data.js
@@ -5,33 +5,44 @@ class Data {
     this.data;
     this.__path__ = path;
     this.__encoding__ = "utf-8";
+
+    this.getData();
   }
 
   async getData() {
-    await new Promise((resolve) => {
+    await this.#readData();
+    return this.data;
+  }
+
+  async setData(data) {
+    const key = data.id;
+
+    if (!this.data[key]) {
+      this.data[key] = data;
+      await this.#writeData();
+    }
+
+    return this.getData();
+  }
+
+  async #readData() {
+    return new Promise((resolve, reject) => {
       fs.readFile(this.__path__, this.__encoding__, (err, data) => {
-        if (err) return 0;
+        if (err) reject(false);
         this.data = JSON.parse(data);
         resolve(true);
       });
     });
-
-    return this.data;
   }
-  async setData(data) {
-    const key = data.id;
 
-    if (this.data[key]) return 0;
-
-    this.data[key] = data;
-
-    await new Promise((resolve) => {
+  async #writeData() {
+    return new Promise((resolve, reject) => {
       fs.writeFile(
         this.__path__,
         JSON.stringify(this.data, null, 2),
         this.__encoding__,
         (err) => {
-          if (err) return 0;
+          if (err) reject(false);
           resolve(true);
         }
       );
