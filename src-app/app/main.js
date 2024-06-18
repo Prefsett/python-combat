8c1637f196452f17f054899dc63fa2fdd15d92de
Prefsett <oleg2008@gmail.com>
2024-06-18 20:36:37 +0300

    feat

--- src-app/app/main.js
+++ src-app/app/main.js
@@ -9,7 +9,7 @@ export class App {
 
     this.#render();
 
-    clickedElement.addEventListener('click', App.#click);
+    clickedElement.addEventListener('touchend', App.#click);
   }
 
   static #render() {
